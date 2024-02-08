a747f7d6042aa7998d14355f25bdedc5adb1641c
Johnny-Nguy <113134683+Johnny-Nguy@users.noreply.github.com>
2024-02-08 14:17:05 -0800

    Update style.js

--- style.js
+++ style.js
@@ -1,1 +1,28 @@
+const style = {
+    cookie: {
+        marginLeft: 500,
+        marginTop: 100,
+        padding: 0,
+        width: 0,
+        height: 0,
+    },
+    inputGroup: {
+        marginBottom: 10
+    },
+    input: {
+        backgroundColor: "#EFEFFF",
+        marginLeft: 10
+    },
+    error: {
+        color: "red",
+        margin: 5
+    },
+    label:{
+        marginLeft: 700,
+    },
+    reset:{
+        marginLeft: 720,
+    }
+};
 
+export default style;
