5e604d21c588d40920c1373463913ac9569acba9
Johnny-Nguy <113134683+Johnny-Nguy@users.noreply.github.com>
2024-02-14 22:57:44 -0800

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -1,5 +1,5 @@
-import style from './style';
 import React from "react";
+import style from './style';
 
 class Cookie extends React.Component {
   state = {
@@ -7,35 +7,36 @@ class Cookie extends React.Component {
   };
 
   Add = () => {
-    this.setState ({ score: this.state.score + 1 });
-  }
+    this.setState({ score: this.state.score + 1 });
+  };
 
   Reset = () => {
-    this.setState ({score: 0});
-  } 
+    this.setState({ score: 0 });
+  };
 
   render() {
-    return (  
+    return (
       <div>
+        // style.cookie calls cookie method from style.js
         <button style={style.cookie} onClick={this.Add}>
-          <img 
-            src="https://pbs.twimg.com/profile_images/1691979424923561984/IdhzQ7-H_400x400.jpg"
-            style={{ width: '500px', height: '500px' }}  // Adjust the width and height here
-            alt="cookie"
+          <img
+            src="https://famouscookies.com/wp-content/uploads/2020/11/Famous-Cookie-Cocolate-Chip-21-1600x1600-1.png"
+            style={{ width: '500px', height: '500px' }}
+            alt="Cookie can't be loaded :("
           />
         </button>
-        <div>
-          <font size="10" style={style.label}>
-            wah-wahs = {this.state.score}
-          </font>
+        // ... operator to apply additional stuff, sets location and size of label on page
+        <div style={{ fontSize: '50px', ...style.label }}>
+          clicks = {this.state.score}
         </div>
+        // style.reset calls reset method from style.js
         <button style={style.reset} onClick={this.Reset}>
           Click to reset cookies
         </button>
       </div>
+      // all buttons list its functions in the <> then what it displays after
     );
   }
 }
 
 export default Cookie;
-
